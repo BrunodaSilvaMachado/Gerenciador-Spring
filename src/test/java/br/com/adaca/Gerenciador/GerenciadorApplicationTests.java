5d762f9b90bac32c6a413f97a25c9f81f53725a5
Willian Medeiros de Faria <willianmfaria@users.noreply.github.com>
2018-08-27 14:14:13 -0300

    Código inicial

---
+++ src/test/java/br/com/adaca/Gerenciador/GerenciadorApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.adaca.Gerenciador;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GerenciadorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

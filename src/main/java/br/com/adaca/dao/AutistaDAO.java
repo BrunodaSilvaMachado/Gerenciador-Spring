7c1e0cb8182182b43c7e12ea691936a3d499005d
Willian Medeiros de Faria <willianmfaria@users.noreply.github.com>
2018-09-03 10:53:18 -0300

    Pequenos ajustes

--- src/main/java/br/com/adaca/dao/AutistaDAO.java
+++ src/main/java/br/com/adaca/dao/AutistaDAO.java
@@ -15,36 +15,6 @@ public class AutistaDAO {
     @Autowired
     private AutistaRepository autistaRepository;
 
-
-    /**
-     * Lista todas as crianças cadastradas no banco de dados
-     *
-     * @return Lista com todas as crianças cadastradas
-     */
-    public List<Autista> listar() {
-        List<Autista> autistas = new ArrayList<>();
-        Iterator<Autista> iterator = autistaRepository.findAll().iterator();
-        while (iterator.hasNext()) {
-            autistas.add(iterator.next());
-        }
-        return autistas;
-    }
-
-    /**
-     * Efetua uma busca por ID da criança cadastrada
-     *
-     * @param autistaId ID da criança já existente no banco de dados
-     * @return Objeto da criança encontrada
-     */
-    public Autista selecionar(Integer autistaId) {
-        Optional<Autista> autista = autistaRepository.findById(autistaId);
-        if (autista.isPresent()) {
-            return autista.get();
-        } else {
-            return null;
-        }
-    }
-
     /**
      * Salva o cadastro da criança no banco de dados
      *
@@ -52,16 +22,6 @@ public class AutistaDAO {
      * @return Erro ou sucesso ao salvar
      */
     public Autista salvar(Autista autista) {
-        return autistaRepository.save(autista);
-    }
-
-    /**
-     * Altera o cadastro da criança no bando de dados
-     *
-     * @param autista Objeto preenchido com os dados já alterados
-     * @return Erro ou sucesso ao alterar
-     */
-    public Autista alterar(Autista autista) {
         return autistaRepository.save(autista);
     }
 
@@ -80,6 +40,66 @@ public class AutistaDAO {
         } else {
             return false;
         }
+    }
+
+    /**
+     * Remove o cadastro da criança do banco de dados
+     *
+     * @param autista Objeto preenchido do cadastro já existente no banco de
+     * dados
+     * @return Erro ou sucesso ao remover
+     */
+    public Boolean remover(Autista autista) {
+        autistaRepository.delete(autista);
+        return true;
+    }
+
+    /**
+     * Altera o cadastro da criança no bando de dados
+     *
+     * @param autista Objeto preenchido com os dados já alterados
+     * @return Erro ou sucesso ao alterar
+     */
+    public Autista alterar(Autista autista) {
+        return autistaRepository.save(autista);
+    }
+
+    /**
+     * Efetua uma busca por ID da criança cadastrada
+     *
+     * @param autistaId ID da criança já existente no banco de dados
+     * @return Objeto da criança encontrada
+     */
+    public Autista selecionar(Integer autistaId) {
+        Optional<Autista> autista = autistaRepository.findById(autistaId);
+        if (autista.isPresent()) {
+            return autista.get();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Lista todas as crianças cadastradas no banco de dados
+     *
+     * @return Lista com todas as crianças cadastradas
+     */
+    public List<Autista> listar() {
+        List<Autista> autistas = new ArrayList<>();
+        Iterator<Autista> iterator = autistaRepository.findAll().iterator();
+        while (iterator.hasNext()) {
+            autistas.add(iterator.next());
+        }
+        return autistas;
+    }
+
+    /**
+     * Lista id e nome de todas as crianças cadastradas no banco de dados
+     *
+     * @return Lista de ids e nomes de todas as crianças cadastradas
+     */
+    public List<Autista> listarNomesId() {
+        return autistaRepository.listNamesId();
     }
 }
 
--- src/main/java/br/com/adaca/repository/AutistaRepository.java
+++ src/main/java/br/com/adaca/repository/AutistaRepository.java
@@ -1,10 +1,14 @@
 package br.com.adaca.repository;
 
 import br.com.adaca.entity.Autista;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.io.Serializable;
+import java.util.List;
 
 public interface AutistaRepository extends CrudRepository<Autista, Serializable> {
 
+    @Query("SELECT a.id, a.nome FROM Autista a")
+    public List<Autista> listNamesId();
 }

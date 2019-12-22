0d847829bdd89136e401af780acc4d976ad6de51
BrunodaSilvaMachado <brunosilvamachado@id.uff.br>
2019-12-22 13:33:46 -0300

    Metodo lestarTutores foi removido

--- src/main/java/br/com/adaca/repository/TutorRepository.java
+++ src/main/java/br/com/adaca/repository/TutorRepository.java
@@ -6,13 +6,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.io.Serializable;
-import java.util.List;
 
 public interface TutorRepository extends CrudRepository<Tutor, Serializable>  {
 
     @Query("SELECT t FROM Tutor t WHERE upper(t.usuario) = :usuario AND t.senha = :senha")
     public Tutor login(@Param("usuario") String usuario, @Param("senha") String senha);
-
-    @Query("SELECT t.id, t.nome, t.usuario, t.area FROM Tutor t")
-    public List<Tutor> listTutores();
 }

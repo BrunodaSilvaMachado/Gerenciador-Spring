59ae8d18d2090965496eb0d6b5e6766d0b68e10c
BrunodaSilvaMachado <brunosilvamachado@id.uff.br>
2019-12-22 14:40:38 -0300

    add resource handler

--- src/main/java/br/com/adaca/config/WebConfig.java
+++ src/main/java/br/com/adaca/config/WebConfig.java
@@ -3,6 +3,7 @@ package br.com.adaca.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,5 +13,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedMethods("GET","POST","PUT","DELETE","OPTIONS","HEAD","TRACE","CONNECT");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**", "/js/**")
+                .addResourceLocations("classpath:/static/css/", "classpath:/static/js/");
     }
 }

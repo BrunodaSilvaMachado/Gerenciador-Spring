c1ca78fd06d924c5d469001033dc14744139280a
Bruno da silva <brunosilvamachado@id.uff.br>
2019-12-31 19:38:56 -0300

    Password encoder foi movido para SecurityConfig

--- src/main/java/br/com/adaca/GerenciadorApplication.java
+++ src/main/java/br/com/adaca/GerenciadorApplication.java
@@ -1,30 +1,23 @@
 package br.com.adaca;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableJpaRepositories(basePackages = {"br.com.adaca.repository"})
 @EntityScan(basePackages = {"br.com.adaca.model"})
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class GerenciadorApplication extends SpringBootServletInitializer {
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciadorApplication.class, args);
     }
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(GerenciadorApplication.class);
     }
-
-    public static void main(String[] args) {
-		SpringApplication.run(GerenciadorApplication.class, args);
-	}
 }

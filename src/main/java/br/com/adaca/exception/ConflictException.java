085d933ff1693b17c192cc36bb706a1967e737c0
Bruno da silva <brunosilvamachado@id.uff.br>
2019-12-31 20:09:38 -0300

    Update Exceptions

--- src/main/java/br/com/adaca/exception/ConflictException.java
+++ src/main/java/br/com/adaca/exception/ConflictException.java
@@ -1,8 +1,5 @@
 package br.com.adaca.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 //@ResponseStatus(HttpStatus.CONFLICT)
 public class ConflictException extends RuntimeException {
 
--- src/main/java/br/com/adaca/exception/EmptyException.java
+++ src/main/java/br/com/adaca/exception/EmptyException.java
@@ -1,11 +1,13 @@
 package br.com.adaca.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 //@ResponseStatus(HttpStatus.CONFLICT)
 public class EmptyException extends RuntimeException {
 
-    public EmptyException(String message) { super(message); }
-    public EmptyException(){ super(); }
+    public EmptyException(String message) {
+        super(message);
+    }
+
+    public EmptyException() {
+        super();
+    }
 }
